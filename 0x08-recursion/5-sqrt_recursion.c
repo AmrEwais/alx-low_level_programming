aa9e75d816b186c6788f1dc2ffcdc1154461978e
AmrEwais <amrewais52@gmail.com>
2023-04-06 22:25:56 +0000

    2nd Try

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,30 @@
 #include "main.h"
 
 /**
-* squareRoot - function that returns the natural square root of a number.
-* @num: input number.
-* @1: for guss.
-* Return: square root.
-*/
+ * get_sqrt - get the square root
+ * @a:int
+ * @b:int
+ * Return: int
+ */
 
-double squareRoot(double num, double guess = 1)
+int get_sqrt(int a, int b)
 {
-	if (abs(guess * guess - num) < 0.0001)
-	{
-		return (guess);
-	}
-
-	double newGuess = (guess + num / guess) / 2;
-
-	return (squareRoot(num, newGuess));
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (get_sqrt(a + 1, b));
 }
 
 /**
-* _sqrt_recursion - function that returns the natural square root of a number.
-* @n: input number.
-*
-* Return: square root of @n.
-*/
+ * _sqrt_recursion - function returns x^y.
+ * @n: input integer.
+ * Return: natural square root or -1
+ */
 
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	return (squareRoot(n, 1));
+	return (get_sqrt(1, n));
 }
